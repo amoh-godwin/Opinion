0cb1c12c0aef694289b896075e15f0ce086cce56
Amoh - Gyebi Godwin <amohgyebigodwin@gmail.com>
2017-12-16 15:59:59 +0000

    Add files via upload

--- conn.php
+++ conn.php
@@ -6,9 +6,9 @@
  * Time: 7:10 PM
  */
 $host = "localhost";
-$user = "root";
-$pass = "ampofo1";
-$db_name = "design";
+$user = "id2266769_amohgodwin";
+$pass = "africa16";
+$db_name = "id2266769_opinon";
 
 if($conn = new mysqli($host, $user, $pass, $db_name)) {
 } else {
