d9b409e6d5b779d76603a0d01fe4d5e7f6a99512
Amoh - Gyebi Godwin <amohgyebigodwin@gmail.com>
2017-12-16 16:00:47 +0000

    Update conn.php

--- conn.php
+++ conn.php
@@ -6,9 +6,9 @@
  * Time: 7:10 PM
  */
 $host = "localhost";
-$user = "id2266769_amohgodwin";
-$pass = "africa16";
-$db_name = "id2266769_opinon";
+$user = "";
+$pass = "";
+$db_name = "";
 
 if($conn = new mysqli($host, $user, $pass, $db_name)) {
 } else {
