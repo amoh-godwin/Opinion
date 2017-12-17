d032dcca212ee650dc27b95c5b8401b1819b90a7
Amoh - Gyebi Godwin <amohgyebigodwin@gmail.com>
2017-12-17 19:56:31 +0000

    Add files via upload

---
+++ id1756130_youthalive.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.7.6
+ */
+
+/**
+ * Database `dbname_here`
+ */
+
+/* `dbname_here`.`admin_menu` */
+$admin_menu = array(
+  array('id' => '1','name' => 'Post','link' => 'view_post','admin_id' => '1'),
+  array('id' => '2','name' => 'Create Post','link' => 'create_post','admin_id' => '1'),
+  array('id' => '3','name' => 'Series','link' => 'series','admin_id' => '1'),
+  array('id' => '4','name' => 'Token','link' => 'token','admin_id' => '1'),
+  array('id' => '5','name' => 'Priviledges','link' => 'priviledges','admin_id' => '1'),
+  array('id' => '6','name' => 'Create Menu','link' => 'menu','admin_id' => '1'),
+  array('id' => '7','name' => 'Account','link' => 'account_info','admin_id' => '1'),
+  array('id' => '8','name' => 'Log Out','link' => 'log_out','admin_id' => '0'),
+  array('id' => '9','name' => 'Post','link' => 'view_post','admin_id' => '1'),
+  array('id' => '10','name' => 'Create Post','link' => 'create_post','admin_id' => '1'),
+  array('id' => '11','name' => 'Series','link' => 'series','admin_id' => '1'),
+  array('id' => '12','name' => 'Token','link' => 'token','admin_id' => '1'),
+  array('id' => '13','name' => 'Priviledges','link' => 'priviledges','admin_id' => '1'),
+  array('id' => '14','name' => 'Create Menu','link' => 'menu','admin_id' => '1'),
+  array('id' => '15','name' => 'Account','link' => 'account_info','admin_id' => '1'),
+  array('id' => '16','name' => 'Log Out','link' => 'log_out','admin_id' => '0')
+);
+
+/* `dbname_here`.`contact_us` */
+$contact_us = array(
+);
+
+/* `dbname_here`.`menu` */
+$menu = array(
+  array('id' => '1','name' => 'New Post','link' => 'create_post.html','visible' => '1'),
+  array('id' => '2','name' => 'About Us','link' => 'about_us.html','visible' => '3'),
+  array('id' => '3','name' => 'Contact Us','link' => 'contact_us.html','visible' => '3'),
+  array('id' => '4','name' => 'Log In','link' => 'login.html','visible' => '0'),
+  array('id' => '5','name' => 'Log Out','link' => 'logout.php','visible' => '1'),
+  array('id' => '6','name' => 'Account','link' => 'account.php','visible' => '1')
+);
+
+/* `dbname_here`.`pages` */
+$pages = array(
+);
+
+/* `dbname_here`.`post` */
+$post = array(
+);
+
+/* `dbname_here`.`posts` */
+$posts = array(
+);
+
+/* `dbname_here`.`series` */
+$series = array(
+);
+
+/* `dbname_here`.`session` */
+$session = array(
+);
+
+/* `dbname_here`.`token` */
+$token = array(
+);
+
+/* `dbname_here`.`users` */
+$users = array(
+  array('id' => '1','fname' => '','lname' => '','email' => 'ferdinand@gmail.com','username' => 'ferdinand','password_length' => '8','stwotsrd' => 'rd','hashed_password' => '$2a$07$thensallysaltygoeshereUL/JbrpryAFrRs7WeIi7wNJDA77DPkG','image' => 'images/OPINON_1505770386.jpg','admin_id' => '1','date' => '1496438600')
+);
